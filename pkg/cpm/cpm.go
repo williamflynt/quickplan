c7db171b93d39a85efec1fe3994e70bf14812dc0
williamflynt <williamflynt>
2022-06-11 01:14:09 -0700

    feat(activity): Calculate LatestStart/Finish.

--- pkg/cpm/cpm.go
+++ pkg/cpm/cpm.go
@@ -20,23 +20,16 @@ type Node struct {
 	Label          string  `json:"label"`
 	EarliestStart  float64 `json:"earliestStart"`
 	EarliestFinish float64 `json:"earliestFinish"`
+	LatestStart    float64 `json:"latestStart"`
+	LatestFinish   float64 `json:"latestFinish"`
 	Slack          float64 `json:"slack"`
 
-	task         Task
-	dependsOn    []*Node
-	isDepOf      []*Node
-	calcComplete bool
-	lock         sync.Mutex
-}
-
-func (n *Node) IsDepOf(nn *Node) {
-	// Assumes no duplicates!
-	n.isDepOf = append(n.isDepOf, nn)
-}
-
-func (n *Node) DependsOn(nn *Node) {
-	// Assumes no duplicates!
-	n.dependsOn = append(n.dependsOn, nn)
+	task              Task
+	dependsOn         []*Node // dependsOn contains the Nodes that this one needs to run first.
+	requiredBy        []*Node // requiredBy contains the Nodes that require this one to run first.
+	earlyCalcComplete bool
+	lateCalcComplete  bool
+	lock              sync.Mutex
 }
 
 type Arrow struct {
@@ -54,21 +47,28 @@ type Chart struct {
 
 func Calculate(tasks []Task) (chart Chart, err error) {
 
-	starts := chart.buildNodesArrows(tasks)
+	starts, ends := chart.buildNodesArrows(tasks)
 	if len(starts) == 0 {
 		return chart, errors.New("cycle detected - no starts found")
 	}
 
+	// Forward pass.
 	for _, n := range starts {
-		calculateStartFinish(n)
+		calculateEarlyStartFinish(n)
+	}
+	// Backward pass.
+	for _, n := range ends {
+		chart.calculateLateStartFinish(n)
 	}
 
-	err = chart.findCriticalPath()
+	err = chart.findCriticalPath(ends)
 	return chart, err
 }
 
-func (c *Chart) buildNodesArrows(tasks []Task) []*Node {
+func (c *Chart) buildNodesArrows(tasks []Task) (starts []*Node, ends []*Node) {
+	// Mapping of Task ID to Node pointer.
 	nodes := make(map[string]*Node)
+	// Mapping of Task ID to Task ID.
 	arrowIds := make(map[string]string)
 
 	for _, t := range tasks {
@@ -77,22 +77,30 @@ func (c *Chart) buildNodesArrows(tasks []Task) []*Node {
 		c.Nodes = append(c.Nodes, n)
 	}
 
+	// We've gone through all Tasks, so all should have Nodes.
 	for _, t := range tasks {
+		nt := nodes[t.Uid()]
 		for _, p := range t.Predecessors() {
-			n := nodes[t.Uid()]
-			n.DependsOn(nodes[p.Uid()])
-			arrowIds[t.Uid()] = p.Uid()
+			np := nodes[p.Uid()]
+			// Get this Task's Node, then add its Predecessors to `dependsOn`.
+			nt.dependsOn = append(nt.dependsOn, np)
+			// Also add this Task to the Predecessor's requiredBy.
+			np.requiredBy = append(np.requiredBy, nt)
+			arrowIds[nt.Id] = np.Id
 		}
 	}
 
-	starts := make([]*Node, 0)
+	// Get the starter and ender Nodes.
 	for _, n := range c.Nodes {
+		// If it depends on nothing, it's a starter Node.
 		if len(n.dependsOn) == 0 {
 			starts = append(starts, n)
 			continue
 		}
-		for _, d := range n.dependsOn {
-			d.IsDepOf(n)
+		// If it's requiredBy nothing, it's an ender Node.
+		if len(n.requiredBy) == 0 {
+			ends = append(ends, n)
+			continue
 		}
 	}
 
@@ -104,24 +112,10 @@ func (c *Chart) buildNodesArrows(tasks []Task) []*Node {
 	}
 	c.Arrows = arrows
 
-	return starts
+	return starts, ends
 }
 
-func (c *Chart) findCriticalPath() error {
-	ends := make([]*Node, 0)
-	maxFin := float64(0)
-	for _, n := range c.Nodes {
-		if len(n.isDepOf) == 0 {
-			if n.EarliestFinish < maxFin {
-				continue
-			}
-			if n.EarliestFinish > maxFin {
-				ends = []*Node{n}
-				continue
-			}
-			ends = append(ends, n)
-		}
-	}
+func (c *Chart) findCriticalPath(ends []*Node) error {
 	if len(ends) == 0 {
 		return errors.New("cycle detected - no ends found")
 	}
@@ -153,26 +147,66 @@ func addToCriticalPath(node *Node, arrowIds map[string]struct{}) map[string]stru
 	return arrowIds
 }
 
-func calculateStartFinish(n *Node) {
+// calculateEarlyStartFinish does a forward pass over the Nodes in the Chart to calculate
+// the earliest start and finish Duration - prior to finding the critical path.
+func calculateEarlyStartFinish(n *Node) {
 	n.lock.Lock()
-	if n.calcComplete {
+	if n.earlyCalcComplete {
+		n.lock.Unlock()
 		return
 	}
-	for _, nn := range n.dependsOn {
-		if n.EarliestStart < nn.EarliestFinish {
-			if !nn.calcComplete {
-				calculateStartFinish(nn)
+	// Find this Node's earliest start time based on the earliest finish time of upstream.
+	// This loop finds the greatest upstream finish time and uses it.
+	for _, lookBack := range n.dependsOn {
+		// If this Node's earliest start is before the upstream earliest finish, that's not possible.
+		// Run the calc for that Node (if not already done), and set this Node's earliest start time.
+		if n.EarliestStart < lookBack.EarliestFinish {
+			if !lookBack.earlyCalcComplete {
+				calculateEarlyStartFinish(lookBack)
 			}
-			n.EarliestStart = nn.EarliestFinish
+			n.EarliestStart = lookBack.EarliestFinish
 		}
 	}
+
 	n.EarliestFinish = n.EarliestStart + n.Duration
-	n.calcComplete = true
+	n.earlyCalcComplete = true
 	n.lock.Unlock()
-	// Recur through the graph.
-	for _, nn := range n.isDepOf {
-		calculateStartFinish(nn)
+	// Recur forward through the graph.
+	for _, lookAhead := range n.requiredBy {
+		calculateEarlyStartFinish(lookAhead)
 	}
+}
+
+// calculateLateStartFinish does a backward pass over the Nodes in the Chart to calculate
+// the latest start and finish Duration.
+func (c *Chart) calculateLateStartFinish(n *Node) {
+	n.lock.Lock()
+	defer n.lock.Unlock()
+
+	if len(n.dependsOn) == 0 {
+		// This is a starter Node.
+		n.lateCalcComplete = true
+		return
+	}
+	if len(n.requiredBy) == 0 {
+		// This is an ender Node. The below is just in case we have multiple ender Nodes.
+		maxFinChart := findLatestNodes(c.Nodes)[0].EarliestFinish
+		n.LatestFinish = maxFinChart
+		n.LatestStart = n.LatestFinish - n.Duration
+	}
+
+	// Find the Nodes that require this one (lookBack) and get the latest finish.
+	maxFin := findLatestNodes(n.dependsOn)[0].EarliestFinish
+	// Given that latest finish, apply it to get latest values for each lookBack Node.
+	for _, lookBack := range n.dependsOn {
+		if lookBack.lateCalcComplete {
+			return
+		}
+		lookBack.LatestFinish = maxFin - 1
+		lookBack.LatestStart = lookBack.LatestFinish - lookBack.Duration
+		c.calculateLateStartFinish(lookBack)
+	}
+	n.lateCalcComplete = true
 }
 
 func findLatestNodes(nodes []*Node) (latest []*Node) {
@@ -198,10 +232,12 @@ func nodeFromTask(t Task) *Node {
 		Duration:       t.Duration(),
 		Label:          t.Label(),
 		EarliestStart:  0,
-		EarliestFinish: 0,
+		EarliestFinish: t.Duration(),
+		LatestStart:    0,
+		LatestFinish:   t.Duration(),
 		Slack:          0,
 		task:           t,
 		dependsOn:      before,
-		isDepOf:        after,
+		requiredBy:     after,
 	}
 }
