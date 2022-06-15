e1229b2faa10cbf97af14fda6c059b270db9af42
williamflynt <williamflynt>
2022-06-14 18:42:39 -0700

    feat(http-impl): Save graph to disk on Save.

--- pkg/activity/store.go
+++ pkg/activity/store.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/cockroachdb/errors"
 	"github.com/rs/zerolog/log"
+	"os"
 )
 
 // GraphStore describes how we will save, load, and delete Graphs.
@@ -25,7 +26,7 @@ func NewInMemoryGraphStore() *InMemoryGraphStore {
 	return &InMemoryGraphStore{storage: make(map[string]Graph)}
 }
 
-// Save adds the Graph to the store, but also saves the Chart JSON to `/tmp/<id>.json` as a backup measure.
+// Save adds the Graph to the store, but also saves the Chart JSON to `/tmp/<id>.graph` as a backup measure.
 func (s *InMemoryGraphStore) Save(graph Graph) (string, error) {
 	if graph == nil {
 		return "", errors.New("cannot store Graph with nil pointer")
@@ -35,7 +36,9 @@ func (s *InMemoryGraphStore) Save(graph Graph) (string, error) {
 		id = fmt.Sprintf(`%v`, graph)
 	}
 	s.storage[id] = graph
-	return id, nil
+	b := graph.Serialize()
+	err := writeTmpGraphFile(id, b)
+	return id, err
 }
 
 func (s *InMemoryGraphStore) List() []string {
@@ -57,4 +60,25 @@ func (s *InMemoryGraphStore) Load(id string) (Graph, error) {
 
 func (s *InMemoryGraphStore) Delete(id string) {
 	delete(s.storage, id)
+}
+
+// --- HELPERS ---
+
+func writeTmpGraphFile(id string, data []byte) error {
+	path := fmt.Sprintf(`/tmp/%s.graph`, id)
+	file, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE, 0644)
+	if err != nil {
+		return err
+	}
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			log.Error().Err(err).Msg("failed to close file")
+		}
+	}(file)
+	if _, err := file.Write(data); err != nil {
+		log.Error().Err(err).Msg("failed to write to file")
+		return err
+	}
+	return nil
 }
