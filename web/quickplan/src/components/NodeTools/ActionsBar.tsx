407d25a8f41aa9cb25530c0c5f02a0f63cfdb7b9
william <will@getivydata.com>
2022-06-16 11:20:25 -0700

    feat(position-hold): Delete Dependency.

--- web/quickplan/src/components/NodeTools/ActionsBar.tsx
+++ web/quickplan/src/components/NodeTools/ActionsBar.tsx
@@ -6,6 +6,7 @@ import {Button, Space} from "antd";
 import {ActivityAddButton} from "./ActivityAddButton";
 import {LeftOutlined, RightOutlined} from "@ant-design/icons";
 import {ActivityDeleteButton} from "./ActivityDeleteButton";
+import {EdgeDeleteButton} from "./EdgeDeleteButton";
 
 type ActionsBarProps = {
     node: CpmNodeType | null
@@ -24,15 +25,15 @@ export const ActionsBar: FC<ActionsBarProps> = ({node, edge}) => {
             <ActivityAddButton graphId={activeChartId}/>
             {node &&
                 <>
-                    <Button ghost type="primary"><LeftOutlined/> Insert Before</Button>
-                    <Button ghost type="primary"><RightOutlined/> Insert After</Button>
+                    <Button size="small" ghost type="primary"><LeftOutlined/> Insert Before</Button>
+                    <Button size="small" ghost type="primary"><RightOutlined/> Insert After</Button>
                     <ActivityDeleteButton graphId={activeChartId} node={node}/>
                 </>
             }
             {edge &&
                 <>
-                    <Button type="dashed">Split {edge.id}</Button>
-                    <Button danger type="dashed">Delete {edge.id}</Button>
+                    <Button size="small" type="dashed">Split {edge.id}</Button>
+                    <EdgeDeleteButton graphId={activeChartId} edge={edge}/>
                 </>
             }
         </Space>
