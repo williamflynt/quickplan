4e5fcf502b46f5479847ae644ab2e2b6e428c429
williamflynt <williamflynt>
2022-07-06 18:26:51 -0700

    fix(imports): Attempt fix for low/high-only duration estimates.

--- pkg/activity/activity.go
+++ pkg/activity/activity.go
@@ -33,6 +33,23 @@ func (a *Activity) Meta() map[string]string {
 
 // Duration calculates the estimated duration for an Activity.
 func (a *Activity) Duration() float64 {
+	// TODO: Update duration tracking to understand "set" vs. "not set". (wf 6 Jul 22)
+	if (a.DurationLow > 0 || a.DurationLikely > 0 || a.DurationHigh > 0) &&
+		!(a.DurationLikely > 0 && a.DurationHigh > 0) {
+		// They set something, but not the other things. Just do the average of "things that are set".
+		// Don't check DurationLow in the && clause because it might legit be zero.
+		setCount := 1 // We will always count the low value (even if it's zero).
+		sum := 0
+		for _, v := range []int{a.DurationLikely, a.DurationHigh} {
+			if v > 0 {
+				sum += v
+				setCount += 1
+			}
+		}
+		sum += a.DurationLow
+		return tenthRound(float64(sum) / float64(setCount))
+	}
+	// The standard CPM duration figure.
 	ans := float64(a.DurationLow+(a.DurationLikely*4)+a.DurationHigh) / 6
 	// Round to nearest tenth.
 	return tenthRound(ans)
